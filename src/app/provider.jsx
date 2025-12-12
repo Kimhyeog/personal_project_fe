efd9c1c2c42ac166d8eefcb05fcc53517e40df4f
김형준 <>
2025-12-12 16:21:42 +0900

    TanStack Query Provider 세팅

---
+++ src/app/provider.jsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { useState } from 'react'
+
+import { QueryClient , QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+export default function Providers({children}) {
+    
+    const [queryClient] = useState(()=>new QueryClient({
+        defaultOptions: {
+          queries: {
+            // 데이터가 stale(상함) 상태로 변하는 시간 (1분)
+            staleTime: 60 * 1000,
+            // 윈도우 포커스 시 자동 재요청 방지
+            refetchOnWindowFocus: false,
+            // API 에러 시 재시도 횟수
+            retry: 1,
+          },
+        },
+
+    }))
+    return (
+    <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
