e2bf090a9fcbc2e51b38c05e84aa719b9db4fdf7
김형준 <>
2025-12-13 01:52:54 +0900

    nav와 검색바 컴포넌트 추가

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,19 +1,37 @@
 import Link from 'next/link'
-import React from 'react'
 import { Button } from '@/components/ui/button'
+
+import Image from 'next/image'
+
+import tistoryLogo from '@/assets/tistory_logo.svg'
+import SearchBar from './SearchBar'
+import Mainnav from './Mainnav'
 
 const Header = () => {
   return (
-    <header>
-        <Link href={"/"} className=''>
-        Board Project
+    <header className="flex w-full items-center justify-between px-10 py-6">
+      <div>
+        <Link href={'/'} className="">
+          <Image
+            src={tistoryLogo}
+            alt="Tistory Logo"
+            width={120}
+            height={30}
+            priority // (선택) 헤더 로고는 LCP(최대 콘텐츠 풀 페인트) 성능을 위해 미리 로드 추천
+          />
         </Link>
-        <Link href={"/login"}>
-            <Button variant={"outline"} size={"sm"}>로그인</Button>
+      </div>
+      <div>
+        <Mainnav />
+      </div>
+      <div>
+        <SearchBar />
+      </div>
+      <div className="flex items-center justify-center gap-3">
+        <Link href={'/login'}>
+          <Button size={'sm'}>시작하기</Button>
         </Link>
-        <Link href={"/singup"}>
-            <Button size={"sm"}>로그인</Button>
-        </Link>
+      </div>
     </header>
   )
 }
